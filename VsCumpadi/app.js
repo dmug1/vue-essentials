40e9677622a824e8bf2bea8548d6031d12d100e6
Diego <diegomugnae@gmail.com>
2020-10-13 15:16:30 +1000

    add deleting and showing total of items

--- VsCumpadi/app.js
+++ VsCumpadi/app.js
@@ -44,13 +44,51 @@
       }
     },
     computed:{
+      cartQty: function(){
+        let qty = 0;
+        for (key in this.cart){
+          qty = qty + this.cart[key].qty
+        }
+        return qty;
+      },
+
+      cartTotal: function(){
+        let sum = 0;
+        for (key in this.cart){
+          sum = sum + (this.cart[key].product.price * this.cart[key].qty);
+        }
+        return sum;
+      },
       sliderState: function(){
         return this.sliderStatus ? 'd-flex' : 'd-none'
       }
     },
     methods: {
       addItem:function(product){
-        this.cart.push(product)
+        //this.cart.push(product)
+        var whichProduct;
+        var existing = this.cart.filter(function(item, index){
+          if (item.product.id == Number(product.id)){
+            whichProduct = index;
+            return true
+          } else {
+            return false;
+          }
+        });
+
+        if (existing.length) {
+          this.cart[whichProduct].qty++
+        }else{
+          this.cart.push({product: product, qty: 1})
+        }
+
+      },
+      deleteItem: function(id){
+        if(this.cart[id].qty>1){
+          this.cart[id].qty--;
+        }else{
+          this.cart.splice(id, 1);
+        }
       },
       beforeEnter: function(el){
         el.className='d-none'
